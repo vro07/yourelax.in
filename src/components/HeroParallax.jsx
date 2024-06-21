8db1ada7ccdd156ff4f601ec02ac83ccaa9a97f7
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-06-21 23:50:18 +0530

    Hero button fix

--- src/components/HeroParallax.jsx
+++ src/components/HeroParallax.jsx
@@ -103,10 +103,11 @@ export const Header = () => {
         you, ensuring quality and reliability with every selection.
       </p>
       <button
+        style={{ position: "fixed", zIndex: 1000 }}
         onClick={() => {
           navigate("/products");
         }}
-        className="inline-flex h-12 mt-2 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 z-50"
+        className="inline-flex h-12 mt-2 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
       >
         View All Products
       </button>
